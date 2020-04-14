997daecc9a26f399fc8af7c1d4a0fdcea2b46ebd
Norbi <blamro@gmail.com>
2020-04-14 10:21:23 +0200

    first couple of methods

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ class Settings():
         super().__init__()
         '''Initialize the games settings.'''
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
